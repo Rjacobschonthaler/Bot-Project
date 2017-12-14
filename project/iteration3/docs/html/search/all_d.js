2d02d29d42fa8f3465dd27e62ce5487726503105
R. Jacob Schonthaler <schon099@umn.edu>
2017-12-14 02:32:18 -0600

    All done!

--- project/iteration3/docs/html/search/all_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['timestepupdate',['TimestepUpdate',['../class_arena_entity.html#af01b8889f46572c25bae0b23358e03a5',1,'ArenaEntity::TimestepUpdate()'],['../class_home_base.html#a02d49d102b61ba9ad543155263b95de8',1,'HomeBase::TimestepUpdate()']]],
-  ['todo_20list',['Todo List',['../todo.html',1,'']]]
-];
--- project/iteration3/docs/html/search/functions_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['accept',['Accept',['../class_arena.html#a4a677fcedab944a410d58da6385aae5f',1,'Arena::Accept()'],['../class_robot_battery.html#a8836b4c7536ebaeb3e4ae0696688ce4e',1,'RobotBattery::Accept()'],['../class_sensor_touch.html#a598eb6f4d7208b8d62adb8c1973fa8c2',1,'SensorTouch::Accept()']]],
-  ['acceptcommand',['AcceptCommand',['../class_robot_motion_handler.html#a8ccb25224a5105811282cfb94b08a2b2',1,'RobotMotionHandler']]],
-  ['advancetime',['AdvanceTime',['../class_arena.html#a458efec4ba1a72561796caace2bb22b8',1,'Arena']]],
-  ['arena',['Arena',['../class_arena.html#ac442d519facc5feebfd7612a53817e9a',1,'Arena']]]
-];
